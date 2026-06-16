9332c2bb2019449dcb78cd380f25508fde06db72
xandon23 <alexandre.de.j.goncalves@gmail.com>
2026-06-15 23:43:18 -0300

    feat: configura sequelize com variaveis de ambiente e conecta ao mysql no docker

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 module.exports = {
   development: {
-    username: "root",
-    password: null,
-    database: "marketplace_automoveis",
-    host: "localhost",
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: "db",
     dialect: "mysql",
   },
 };

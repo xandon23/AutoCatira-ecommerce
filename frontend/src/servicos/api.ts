261a7f357c4a7bd05142e8c1c1e9e09529f31662
xandon23 <alexandre.de.j.goncalves@gmail.com>
2026-06-18 00:02:35 -0300

    feat: configura nginx como proxy reverso e isola rede do docker

--- frontend/src/servicos/api.ts
+++ frontend/src/servicos/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { IUser, IVehicle, IProposal, IReview, IVehicleImage } from "../types";
 
-export const API_BASE_URL = "http://localhost:3333";
+export const API_BASE_URL = process.env.REACT_APP_API_URL || "/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
